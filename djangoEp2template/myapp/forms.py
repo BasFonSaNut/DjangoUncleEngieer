628fdff800f8c503cc119b39edd02b7d14e909cd
BasFonSaNut <nenomelt@gmail.com>
2022-03-05 15:06:04 +0700

    update form add product

--- djangoEp2template/myapp/forms.py
+++ djangoEp2template/myapp/forms.py
@@ -1,11 +1,15 @@
 from django import forms
-from .models import LogMessage,Employee,Allproduct,GeeksModel
+from .models import LogMessage,Employee,Allproduct,BookProduct,GeeksModel
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Allproduct
         fields = ['name', 'price','description','imageurl','instock','unit','quantity','image']  # NOTE: the trailing comma is required
         
+class BookProductForm(forms.ModelForm):
+    class Meta:
+        model = BookProduct
+        fields = ['name', 'price','author','description','imageurl','imagefilename','instock','unit','quantity','image']  # NOTE: the trailing comma is required
     
 class LogMessageForm(forms.ModelForm):
     class Meta:
