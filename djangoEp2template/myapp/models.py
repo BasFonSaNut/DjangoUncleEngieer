3d86e9e8211b8c5c0252552883730ee8537e35a7
BasFonSaNut <nenomelt@gmail.com>
2022-03-09 14:21:50 +0700

    ืnormalize model to resize image uploaded size

--- djangoEp2template/myapp/models.py
+++ djangoEp2template/myapp/models.py
@@ -12,36 +12,18 @@ class Profile(models.Model):
     photo = models.ImageField(upload_to="photoprofile/",null=True,blank=True,default='default.png')
     usertype = models.CharField(max_length=100,default='member')
     
-    def save(self):
-        super().save()  # saving image first
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)  # saving image first
         img = Image.open(self.photo.path) # Open image using self
 
-        if img.height > 260 or img.width > 171:
-            new_img = (260, 171)
+        if img.height > 200 or img.width > 200:
+            new_img = (200, 200)
             img.thumbnail(new_img)
+            # img.resize(self.photo.path, (200, 200))
             img.save(self.photo.path)  # saving image at the same path
             
     def __str__(self):
         return self.user.first_name
-    
-class LogMessage(models.Model):
-    message = models.CharField(max_length=300)
-    # log_date = models.DateTimeField("date logged")
-    log_date = models.CharField(max_length=100,null=FALSE,blank=FALSE)
-    # name=models.CharField(max_length=100)
-    # price=models.CharField(max_length=100)
-    # description = models.CharField(max_length=500,null=True,blank = True)
-    # imageurl = models.CharField(max_length=500,null=True,blank=True)
-    # instock = models.BooleanField(default=True)
-    # unit = models.CharField(max_length=200,default='-')
-    # quantity = models.IntegerField(default=1)
-    # image = models.ImageField(upload_to="products",null=True,blank=True)
-
-    def __str__(self):
-        """Returns a string representation of a message."""
-        date = timezone.localtime(self.log_date)
-        return f"'{self.message}' logged on {date.strftime('%A, %d %B, %Y at %X')}"
-
  
 class BookProduct(models.Model):
     bookname=models.CharField(max_length=100)
@@ -55,13 +37,14 @@ class BookProduct(models.Model):
     quantity = models.IntegerField(default=1)
     image = models.ImageField(upload_to="books/",null=True,blank=True)
     
-    def save(self):
-        super().save()  # saving image first
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)  # saving image first
         img = Image.open(self.image.path) # Open image using self
 
         if img.height > 260 or img.width > 171:
             new_img = (171, 260)
             img.thumbnail(new_img)
+            # img.resize(self.image.path, (171, 260))
             img.save(self.image.path)  # saving image at the same path
             
     def __self__(self):
@@ -71,6 +54,15 @@ class Employee(models.Model):
     bookname = models.CharField(max_length=50)
     emp_image = models.ImageField(upload_to='upload/')
 
+    
+class LogMessage(models.Model):
+    message = models.CharField(max_length=300)
+    # log_date = models.DateTimeField("date logged")
+    log_date = models.CharField(max_length=100,null=FALSE,blank=FALSE)
+    def __str__(self):
+        """Returns a string representation of a message."""
+        date = timezone.localtime(self.log_date)
+        return f"'{self.message}' logged on {date.strftime('%A, %d %B, %Y at %X')}"
         
 class GeeksModel(models.Model):
     title = models.CharField(max_length = 200)
